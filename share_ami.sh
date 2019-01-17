c1c58468f26bd2f0a0407df4fa83700838e9cf8b
Alex Harvey <alex.harvey@nab.com.au>
2019-01-17 16:45:00 +1100

    Tidy up share_ami.sh

--- share_ami.sh
+++ share_ami.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
   
 usage() {
-  echo "Usage: $0 [-h] SOURCE_IMAGE_ID TARGET_ACCOUNT[,TARGET_ACCOUNT2...]"
+  echo "Usage: $0 [-h] SOURCE_IMAGE_ID \
+TARGET_ACCOUNT[,TARGET_ACCOUNT2...]"
   exit 1
 }
 
@@ -12,19 +13,27 @@ target_account_list=$2
 
 IFS=',' read -r -a target_accounts <<< "$target_account_list"
 
-snapshot_ids=$(aws ec2 describe-images --image-ids $source_image_id \
-  --query 'Images[].BlockDeviceMappings[].Ebs[].SnapshotId' --output text)
+snapshot_ids=$(aws ec2 describe-images \
+  --image-ids $source_image_id \
+  --query \
+    'Images[].BlockDeviceMappings[].Ebs[].SnapshotId' \
+  --output text)
 
 for account in "${target_accounts[@]}" ; do
 
-  echo "Adding launch permission for $account to image $source_image_id..."
+  echo "Adding launch permission for \
+    $account to image $source_image_id..."
+
   aws ec2 modify-image-attribute \
     --image-id $source_image_id \
-    --launch-permission "Add=[{UserId=$account}]"
+    --launch-permission \
+      "Add=[{UserId=$account}]"
 
   for snapshot_id in $snapshot_ids ; do
 
-    echo "Adding create volume permission for $account to snapshot $snapshot_id..."
+    echo "Adding create volume permission \
+      for $account to snapshot $snapshot_id..."
+
     aws ec2 modify-snapshot-attribute \
       --snapshot-id $snapshot_id \
       --attribute createVolumePermission \
